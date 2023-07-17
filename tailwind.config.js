8e095a3accef3dab255dfddfe81610d34d76206d
Sena Durmuş <senad8127@gmail.com>
2023-07-17 19:31:41 +0300

    fixed
    Signed-off-by: Sena Durmuş <senad8127@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens:{
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+
+      },
+    },
+
+    extend: {
+      spacing:{
+        '128' : '32rem',
+      }
+    },
   },
   plugins: [],
+  
 }
 
